d2d0f4e13421a4b32b56eab70ff3e7ff7f864d23
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-04-24 11:48:10 +0545

    add eventquery to update grid box as per suer's request

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Get the grid element
 const grid = document.getElementById("grid");
+
 // default grid size
-const defaultGridSize= 64;
+let gridSize = 16;
 
 // Set the default grid columns and rows
-grid.style.gridTemplateColumns = `repeat(${defaultGridSize}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${defaultGridSize}, 1fr)`;
+grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
 // Calculate the total number of boxes to create
-const totalGridItems = defaultGridSize * defaultGridSize;
+const totalGridItems = gridSize * gridSize;
 
 // Create and append boxes to the grid
 for (let i = 0; i < totalGridItems; i++) {
@@ -16,3 +17,26 @@ for (let i = 0; i < totalGridItems; i++) {
   box.classList.add("grid-item");
   grid.appendChild(box);
 }
+
+// Get the grid slider element
+const gridSlider = document.getElementById("grid-slider");
+
+// Add an event listener to the slider to update the grid size
+gridSlider.addEventListener("input", () => {
+  // Get the value of the slider
+  gridSize = gridSlider.value;
+
+  // Set the grid columns and rows based on the slider value
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  // Remove any existing boxes from the grid
+  grid.innerHTML = "";
+
+  // Create and append new boxes to the grid
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const box = document.createElement("div");
+    box.classList.add("grid-item");
+    grid.appendChild(box);
+  }
+});
